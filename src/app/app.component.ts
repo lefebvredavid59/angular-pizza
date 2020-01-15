fce7af656ba040b08219c6c42b9fa012feb8912b
lefebvredavid59 <dlefebvredev@gmail.com>
2020-01-15 14:35:52 +0100

    Discover Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Pizza {
+  id: number;
+  name: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pizzaparty';
+  title = 'Pizza Party';
+  pizza: Pizza = {
+    id : 1,
+    name: 'Reine',
+    price: 12
+  }
 }
